9fefba52491baa7999202e5ec6708ee3002025c4
TmCrafz <sterzeltim@gmail.com>
2017-09-23 17:56:43 +0200

    Initial commit

---
+++ app/src/main/java/tmcrafz/org/monochromeiconcreator/MainActivity.java
@@ -0,0 +1,13 @@
+package tmcrafz.org.monochromeiconcreator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
